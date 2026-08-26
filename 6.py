2533b39c0c588337b189dfd069dbf3654a29687b
Ronit Mahajan <ronitmahajan47@gmail.com>
2026-08-26 19:25:53 +0530

    DAY 3

---
+++ 6.py
@@ -0,0 +1,6 @@
+movies = []
+movies.append(input("Enter your 1st fav movie: "))
+movies.append(input("Enter your 2nd fav movie: "))
+movies.append(input("Enter your 3rd fav movie: "))
+
+print(movies)
---
+++ 7.py
@@ -0,0 +1,3 @@
+print("KILOMETER to MILES converter =>")
+x = float(input("Enter distance in Kilometer: "))
+print("Distance in Miles = ",x/1.609)
